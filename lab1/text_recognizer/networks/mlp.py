1fbd88429c54521843408a87308f7a744ec091ac
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-03 13:54:54 -0700

    4 layer MLP

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -8,7 +8,7 @@ def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int=128,
         dropout_amount: float=0.2,
-        num_layers: int=3) -> Model:
+        num_layers: int=4) -> Model:
     """
     Simple multi-layer perceptron: just fully-connected layers with dropout between them, with softmax predictions.
     Creates num_layers layers.
@@ -18,6 +18,11 @@ def mlp(input_shape: Tuple[int, ...],
     model = Sequential()
     # Don't forget to pass input_shape to the first layer of the model
     ##### Your code below (Lab 1)
+    model.add(Flatten(input_shape=input_shape))
+    for _ in range(num_layers):
+        model.add(Dense(layer_size, activation='relu'))
+        model.add(Dropout(dropout_amount))
+    model.add(Dense(num_classes, activation='softmax'))
 
     ##### Your code above (Lab 1)
 
