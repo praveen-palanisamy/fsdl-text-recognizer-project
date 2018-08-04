adfa3daf40a42cb4eda54272b2be06904a86eab4
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-04 22:05:57 +0000

    Lab5 exp 5.1 based on best perf model (thus far) from lab 3 & 4

--- lab5/tasks/train_lstm_line_predictor_on_iam.sh
+++ lab5/tasks/train_lstm_line_predictor_on_iam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc"}'
+pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 128, "epochs": 25, "username": "praveen-palanisamy"}}'
