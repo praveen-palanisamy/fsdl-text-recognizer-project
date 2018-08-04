50285c307bba03859b07e5896d12fb15632899bd
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-04 21:32:50 +0000

    Exp 3.1 96.xx accuracy 25 epochs

--- lab3/tasks/train_lstm_line_predictor.sh
+++ lab3/tasks/train_lstm_line_predictor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "EmnistLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size":128, "epochs":20}}'
+pipenv run python training/run_experiment.py --save '{"dataset": "EmnistLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 64, "epochs": 25}}'
