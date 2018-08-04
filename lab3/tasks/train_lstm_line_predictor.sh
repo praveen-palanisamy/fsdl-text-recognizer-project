579bd6e37c41476c4943987961c8e70b1ed6a874
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-04 17:38:21 +0000

    Better weights for lstm with ctc loss. Lab3

--- lab3/tasks/train_lstm_line_predictor.sh
+++ lab3/tasks/train_lstm_line_predictor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --save '{"dataset": "EmnistLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc"}'
+pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "EmnistLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size":128, "epochs":20}}'
