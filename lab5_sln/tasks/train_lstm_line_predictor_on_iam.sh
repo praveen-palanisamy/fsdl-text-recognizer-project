37ef2422ae609f2ea494c960fa1e681c688fb28b
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-05 17:21:50 +0000

    Exp 5.4. Trained weights & state that reached 83.6x % accuracy

--- lab5_sln/tasks/train_lstm_line_predictor_on_iam.sh
+++ lab5_sln/tasks/train_lstm_line_predictor_on_iam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --gpu 1 --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 4, "epochs": 80, "username": "praveen-palanisamy"}}'
+pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 4, "epochs": 10, "username": "praveen-palanisamy"}, "network_args":{"window_stride":2}}'
