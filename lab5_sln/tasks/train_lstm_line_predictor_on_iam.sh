34935d20c4c43059f7b099c7ebad914402c91e50
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-08-05 19:32:55 +0000

    lab5 latest run. commit before moving onto lab6

--- lab5_sln/tasks/train_lstm_line_predictor_on_iam.sh
+++ lab5_sln/tasks/train_lstm_line_predictor_on_iam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 4, "epochs": 10, "username": "praveen-palanisamy"}, "network_args":{"window_stride":2}}'
+pipenv run python training/run_experiment.py --gpu 0 --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args":{"batch_size": 2, "epochs": 15, "username": "praveen-palanisamy"}, "network_args":{"window_stride":1}}'
